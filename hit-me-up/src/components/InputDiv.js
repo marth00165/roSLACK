b2a5743cafa94a5c4df669380f3433f38f72303b
Rohit <rohitpratti@gmail.com>
2019-09-09 08:24:20 -0700

    props added to sideBar

--- hit-me-up/src/components/InputDiv.js
+++ hit-me-up/src/components/InputDiv.js
@@ -1,7 +1,15 @@
+import React from 'react';
 import styled from 'styled-components';
+import { Input } from 'semantic-ui-react';
 
-export default styled.div
-` grid-column: 3;
+const SendMessageWrapper = styled.div`
+  grid-column: 3;
   grid-row: 3;
-
+  margin: 20px;
 `;
+
+export default ({ channelName }) => (
+  <SendMessageWrapper>
+    <Input fluid placeholder={`Message #${channelName}`} />
+  </SendMessageWrapper>
+);
--- hit-me-up/src/routes/ViewTeam.js
+++ hit-me-up/src/routes/ViewTeam.js
@@ -5,24 +5,30 @@ import Header from '../components/Header'
 import Messages from '../components/Messages'
 import InputDiv from '../components/InputDiv'
 import AppLayout from '../components/AppLayout'
-import {Input} from 'semantic-ui-react';
+
 
 
 
 
 export default () => (
   <AppLayout>
-  <Teams>Teams</Teams>
-  <Channels>Channels</Channels>
-  <Header>Header</Header>
+  <Teams
+  teams= {[{id:1, letter:"R"}, {id:2, letter:"O"}, {id:3, letter: "H"}, {id:4, letter: "I"}, {id:5, letter: "T"}]}
+  />
+  <Channels
+    teamName="Team Name"
+    username="username"
+    channels={[{id: 1, name:"General"}, {id:2, name:"Random"}]}
+    users={[{id:1, name:"jawnBot"}, {id:2, name:"boul"}]}
+    />
+
+  <Header channelName= "General"/>
   <Messages>
     <ul>
       <li></li>
       <li></li>
     </ul>
   </Messages>
-  <InputDiv>
-    <Input name="message"  placeholder="Enter Message Here.." fluid />
-  </InputDiv>
+  <InputDiv channelName= "General"/>  
 </AppLayout>
 )
