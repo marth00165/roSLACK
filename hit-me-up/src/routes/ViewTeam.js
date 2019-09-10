40ed63f9c485b5642e66a28524eea68d69167de7
Rohit <rohitpratti@gmail.com>
2019-09-10 09:57:24 -0700

    channels change in the message area once clicked on

--- hit-me-up/src/routes/ViewTeam.js
+++ hit-me-up/src/routes/ViewTeam.js
@@ -1,27 +1,47 @@
 import React from 'react';
-
+import { graphql } from 'react-apollo';
 import Header from '../components/Header'
 import Messages from '../components/Messages'
 import InputDiv from '../components/InputDiv'
 import Sidebar from '../containers/Sidebar'
 import AppLayout from '../components/AppLayout'
+import {allTeamsQuery} from '../graphql/team'
+import findIndex from 'lodash/findIndex';
 
 
 
 
 
- const ViewTeam = ({match: {params}}) => (
-  <AppLayout>
-  <Sidebar currentTeamId={params.team_id} />
-  <Header channelName= "General"/>
-  <Messages>
-    <ul>
-      <li></li>
-      <li></li>
-    </ul>
-  </Messages>
-  <InputDiv channelName= "General"/>
-</AppLayout>
-)
+const ViewTeam = ({ data: { loading, allTeams }, match: { params: { team_id, channel_id } } }) => {
+ if (loading) {
+   return null;
+ }
 
-export default ViewTeam
+ const teamIdx = team_id ? findIndex(allTeams, ['id', parseInt(team_id, 10)]) : 0;
+  const team = allTeams[teamIdx];
+  const channelIdx = channel_id ? findIndex(team.channels, ['id', parseInt(channel_id, 10)]) : 0;
+  const channel = team.channels[channelIdx];
+
+
+   return (
+      <AppLayout>
+      <Sidebar
+        teams={allTeams.map(t => ({
+          id: t.id,
+          letter: t.name.charAt(0).toUpperCase(),
+        }))}
+        team={team}
+      />
+      <Header channelName={channel.name} />
+      <Messages channelId={channel.id}>
+        <ul className="message-list">
+          <li></li>
+          <li></li>
+        </ul>
+      </Messages>
+      <InputDiv channelName={channel.name}/>
+    </AppLayout>
+  )
+}
+
+export default graphql(allTeamsQuery)(ViewTeam);
