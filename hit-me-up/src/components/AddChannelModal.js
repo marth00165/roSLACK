b53a08121118a086f8a9a1cc8747f983d300fe8c
Rohit <rohitpratti@gmail.com>
2019-09-09 19:08:58 -0700

    create channels almost working

---
+++ hit-me-up/src/components/AddChannelModal.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Form, Input, Button, Modal } from 'semantic-ui-react';
+import { withFormik } from 'formik';
+import {gql} from 'apollo-boost';
+import { graphql } from 'react-apollo';
+import {flowRight as compose} from 'lodash'
+
+const AddChannelModal = ({
+  open,
+  onClose,
+  values,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  isSubmitting,
+}) => (
+  <Modal open={open} onClose={onClose}>
+    <Modal.Header>Add Channel</Modal.Header>
+    <Modal.Content>
+      <Form>
+        <Form.Field>
+          <Input
+            value={values.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            name="name"
+            fluid
+            placeholder="Channel name"
+          />
+        </Form.Field>
+        <Form.Group widths="equal">
+          <Button disabled={isSubmitting} fluid onClick={onClose}>
+            Cancel
+          </Button>
+          <Button type="button" disabled={isSubmitting} onClick={handleSubmit} fluid>
+            Create Channel
+          </Button>
+        </Form.Group>
+      </Form>
+    </Modal.Content>
+  </Modal>
+);
+
+const createChannelMutation = gql`
+  mutation($team_id: Int!, $name: String!) {
+    createChannel(team_id: $team_id, name: $name)
+  }
+`;
+
+export default compose(
+  graphql(createChannelMutation),
+  withFormik({
+    mapPropsToValues: () => ({ name: '' }),
+    handleSubmit: async (values, { props: { onClose, team_id, mutate }, setSubmitting }) => {
+      const response = await mutate({ variables: { team_id, name: values.name } });
+      console.log(response)
+      setSubmitting(false);
+    },
+  }),
+)(AddChannelModal);
